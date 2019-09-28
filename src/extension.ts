81fbe057f3106b13a60ef6e2006b05c7fa747edc
Christophe Eymard <christophe.eymard@gmail.com>
2019-09-28 15:09:09 +0200

    removing traces of old vscode-zig extension as we're going to want to be installed alongside it.

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,10 @@
 'use strict';
 import * as vscode from 'vscode';
-import ZigCompilerProvider from './zigCompilerProvider';
-import { ZigFormatProvider, ZigRangeFormatProvider } from './zigFormat';
 import { ZigCompletionProvider } from './zigCompletion';
 
 const ZIG_MODE: vscode.DocumentFilter = { language: 'zig', scheme: 'file' };
 
 export function activate(context: vscode.ExtensionContext) {
-    let compiler = new ZigCompilerProvider();
-    compiler.activate(context.subscriptions);
-    vscode.languages.registerCodeActionsProvider('zig', compiler);
-
-    const zigFormatStatusBar = vscode.window.createStatusBarItem(
-        vscode.StatusBarAlignment.Left,
-    );
-    const logChannel = vscode.window.createOutputChannel('zig');
-    context.subscriptions.push(logChannel);
-    context.subscriptions.push(
-        vscode.languages.registerDocumentFormattingEditProvider(
-            ZIG_MODE,
-            new ZigFormatProvider(logChannel),
-        ),
-    );
-
-    context.subscriptions.push(
-        vscode.languages.registerDocumentRangeFormattingEditProvider(
-            ZIG_MODE,
-            new ZigRangeFormatProvider(logChannel),
-        ),
-    );
 
     const compl_log = vscode.window.createOutputChannel('zig-complete');
     // compl_log.show()
